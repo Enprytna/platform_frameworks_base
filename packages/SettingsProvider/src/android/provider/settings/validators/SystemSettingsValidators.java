2ec23c0a25e6b08d20d2610740f2b3a5f48168ae
ezio84 <brabus84@gmail.com>
2017-09-01 15:28:27 +0200

    base: Double tap to sleep on lockscreen [1/2]
    * @jhonboy121: inject SystemSettings utility class and make use of it
    idoybh (YAAP): Add settings value to the backup array
    
    Change-Id: Id8638b0716abf8b264ea9dc2b8dceb892e019aa0

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -209,5 +209,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.GAMING_MODE_BRIGHTNESS_ENABLED, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.GAMING_MODE_MEDIA_ENABLED, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.GAMING_MODE_MEDIA, new InclusiveIntegerRangeValidator(0, 100));
+        VALIDATORS.put(System.DOUBLE_TAP_SLEEP_LOCKSCREEN, BOOLEAN_VALIDATOR);
     }
 }
